63f73734ae52b5df8d47e660d0d6ca176c6c8c66
rubenscholle <ruben.scholle@outlook.com>
2023-06-11 10:57:12 +0200

    Added websocket events

--- websocketbundle/websocketbundle_helpers.go
+++ websocketbundle/websocketbundle_helpers.go
@@ -1,6 +1,8 @@
 package websocketbundle
 
 import (
+	"encoding/json"
+	"errors"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -12,14 +14,19 @@ func newWsClient(con *websocket.Conn, manager *wsManager) *wsClient {
 	return &wsClient{
 		connection: con,
 		manager:    manager,
+		sends:      make(chan []byte),
 	}
 }
 
 // NewWsManager initializes wsManager object that holds a map with a reference to each client connected
 func newWsManager() *wsManager {
-	return &wsManager{
-		clients: make(wsClientsMap),
+	m := &wsManager{
+		clients:       make(wsClientsMap),
+		messageEvents: make(map[uint]wsMessageEventHandler),
 	}
+	m.setupEventHandlers()
+
+	return m
 }
 
 // serveWs is called by the websocketHandler and handles websocket requests via http
@@ -36,26 +43,30 @@ func (m *wsManager) serveWs(c *gin.Context) {
 
 	m.addClient(client)
 
-	messageType, _, err := wsConn.ReadMessage()
-	if err != nil {
-		log.Println(err)
-		c.Error(err)
-		return
+	go client.readMessage()
+	go client.writeMessages()
+}
+
+// setupEventHandlers configures and adds all handlers
+func (m *wsManager) setupEventHandlers() {
+	m.messageEvents[wsEventSendMessage] = func(message wsMessageEvent, c *wsClient) error {
+		// ToDo
+		log.Println(message)
+		return nil
 	}
+}
 
-	response := []byte("Hello, client!")
-	err = wsConn.WriteMessage(messageType, response)
-	if err != nil {
-		log.Println(err)
-		c.Error(err)
-		return
+// routeWsMessageEvent is used to make sure the correct event goes into the correct handler
+func (m *wsManager) routeWsMessageEvent(message wsMessageEvent, c *wsClient) error {
+	// Check if Handler is present in Map
+	if handler, ok := m.messageEvents[message.MessageType]; ok {
+		if err := handler(message, c); err != nil {
+			return err
+		}
+		return nil
+	} else {
+		return errors.New("event not supported")
 	}
-
-	log.Println(len(m.clients))
-
-	// Todo
-	//go client.readMessages()
-	//go client.writeMessages()
 }
 
 func (m *wsManager) addClient(client *wsClient) {
@@ -77,5 +88,61 @@ func (m *wsManager) removeClient(client *wsClient) {
 	if _, ok := m.clients[client]; ok {
 		client.connection.Close()
 		delete(m.clients, client)
+	}
+}
+
+func (c *wsClient) readMessage() {
+	defer c.manager.removeClient(c)
+
+	for {
+		_, payload, err := c.connection.ReadMessage()
+
+		if err != nil {
+			// to not log disconnects
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Println(err)
+			}
+			break
+		}
+
+		var messageEvent wsMessageEvent
+		if err := json.Unmarshal(payload, &messageEvent); err != nil {
+			log.Println(err)
+			break
+		}
+
+		if err := c.manager.routeWsMessageEvent(messageEvent, c); err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func (c *wsClient) writeMessages() {
+	defer c.manager.removeClient(c)
+
+	for {
+
+		// add other cases later
+		select {
+		case rawMessage, ok := <-c.sends:
+			if !ok {
+				// Manager has closed connection channel
+				if err := c.connection.WriteMessage(websocket.CloseMessage, nil); err != nil {
+					log.Println(err)
+				}
+
+				return
+			}
+
+			message, err := json.Marshal(rawMessage)
+			if err != nil {
+				log.Println(err)
+				return // closes the connection, should we really
+			}
+
+			if err := c.connection.WriteMessage(websocket.TextMessage, message); err != nil {
+				log.Println(err)
+			}
+		}
 	}
 }
--- websocketbundle/websocketbundle_models.go
+++ websocketbundle/websocketbundle_models.go
@@ -6,9 +6,17 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+// Add frontend related events here
+const (
+	wsEventSendMessage = 1
+)
+
 type wsClient struct {
 	connection *websocket.Conn
 	manager    *wsManager
+
+	// avoid multiple send processes at once
+	sends chan []byte
 }
 
 type wsClientsMap map[*wsClient]bool
@@ -19,4 +27,14 @@ type wsManager struct {
 
 	// ToDo use channels instead?
 	sync.RWMutex
+
+	// handlers are functions that are used to handle Events
+	messageEvents map[uint]wsMessageEventHandler
 }
+
+type wsMessageEvent struct {
+	MessageType uint   `json:"message_type"`
+	Content     string `json:"content"`
+}
+
+type wsMessageEventHandler func(message wsMessageEvent, c *wsClient) error
